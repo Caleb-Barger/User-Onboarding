2147c2211fcf47914fd5f3bce6e2d9084db594d1
Caleb Barger <bargercj20@hotmail.com>
2020-04-22 13:16:48 -0700

    added a bit of styling to my form component

--- user-onboarding/src/components/Form.jsx
+++ user-onboarding/src/components/Form.jsx
@@ -3,15 +3,36 @@ import React from 'react'
 const Form = props => {
     const {formValues, changeHandler} = props
     return (
-        <form>
+        <form className='form'>
             <label>
                 Name: <input
                     name='name'
                     value={formValues.name}
                     type='text'
                     onChange={changeHandler}
-
                 />
+                <br/>
+                Email: <input
+                    name='email'
+                    value={formValues.email}
+                    type='email'
+                    onChange={changeHandler}
+                />
+                <br/>
+                Password: <input
+                    name='password'
+                    value={formValues.password}
+                    type='password'
+                    onChange={changeHandler}
+                />
+                <br/>
+                Terms of Service <input
+                    name='termsOfService'
+                    value={formValues.termsOfService}
+                    type='checkbox'
+                    onChange={changeHandler}
+                />
+                <br/>
             </label>
         </form>
     )
